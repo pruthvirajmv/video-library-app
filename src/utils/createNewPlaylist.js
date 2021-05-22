a9e8006caa3f21e093fa80adf98e807fa8e33d59
Pruthviraj-MV <74802690+pruthvirajmv@users.noreply.github.com>
2021-05-22 15:29:59 +0530

    Update createNewPlaylist.js

--- src/utils/createNewPlaylist.js
+++ src/utils/createNewPlaylist.js
@@ -1,20 +1,22 @@
-import axios from "axios";
-import {backendAPI, dispatchTypeEnum, checkError} from "./index";
+import axios from 'axios';
+import { backendAPI, dispatchTypeEnum, checkError } from './index';
 
 export const createNewPlaylist = async (userId, playlistName, dispatch, setIsLoading) => {
-    try{
-        setIsLoading(true);
-        const { data: { success, playlist}} = await axios({
-            method: "POST",
-            url: `${backendAPI.baseURI}/playlists/${userId}`,
-            data: {newPlaylist: playlistName}
-        })
-        if(success){
-            dispatch({type: dispatchTypeEnum.ADD_NEW_PLAYLIST, payload: playlist});
-        }
-    }catch(error){
-        checkError(error);
-    }finally{
-        setIsLoading(false)
+  try {
+    setIsLoading(true);
+    const {
+      data: { success, playlist },
+    } = await axios({
+      method: 'POST',
+      url: `${backendAPI.baseURI}/playlists/${userId}`,
+      data: { playlistName: playlistName },
+    });
+    if (success) {
+      dispatch({ type: dispatchTypeEnum.ADD_NEW_PLAYLIST, payload: playlist });
     }
-}
+  } catch (error) {
+    checkError(error);
+  } finally {
+    setIsLoading(false);
+  }
+};
